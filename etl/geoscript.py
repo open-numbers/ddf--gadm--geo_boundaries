ea4ecb034b5594cdbac55135457a9d1db0cefcf5
finarb <finarb.analytics@gmail.com>
2016-01-19 19:08:50 +0530

    Update V2

--- etl/geoscript.py
+++ etl/geoscript.py
@@ -16,6 +16,7 @@ import datetime
 import ogr
 import csv
 import pandas as pd
+import pandasql as pdsql
 #define root path
 root="/Users/FinArb/Documents/ddf--gadm--geo_boundaries/output"
 #take folder separator as per os
@@ -146,4 +147,67 @@ del csvwriter,lyr,ds
 csvfile.close()
 
 #read back the csv and format the csv
-gadmdata=pd.read_csv(root+pathspt+ "files"+pathspt+"gadm28.csv",iterator=True,header=0)
+gadmdata=pd.read_csv(root+pathspt+ "files"+pathspt+"gadm28.csv",header=0)
+#get country names
+countrydf=gadmdata[['ID_0','ISO','NAME_0']].drop_duplicates()
+countrydf["ISO1"]=countrydf["ISO"]
+countrydf["Varname"]=countrydf["NAME_0"]
+countrydf["Type"]="country"
+countrydf["EngType"]="country"
+countrydf["ParentType"]="continent"
+countrydf["ParentEngType"]="continent"
+countrydf["Layer"]=0
+countrydf["Parent"]="NA"
+#get Layer 1
+layer1df=gadmdata[["ID_1","ISO","HASC_1","NAME_0","NAME_1","VARNAME_1","TYPE_1","ENGTYPE_1"]].drop_duplicates()
+layer1df["Layer"]=1
+layer1df["ParentType"]="country"
+layer1df["ParentEngType"]="country"
+#get Layer 2
+layer2df=gadmdata[["ID_2","ISO","HASC_2","NAME_1","NAME_2","VARNAME_2","TYPE_2","ENGTYPE_2","TYPE_1","ENGTYPE_1"]].drop_duplicates()
+layer2df["Layer"]=2
+#get Layer 3
+layer3df=gadmdata[["ID_3","ISO","HASC_3","NAME_2","NAME_3","VARNAME_3","TYPE_3","ENGTYPE_3","TYPE_2","ENGTYPE_2"]].drop_duplicates()
+layer3df["Layer"]=3
+#get Layer 4
+layer4df=gadmdata[["ID_4","ISO","NAME_3","NAME_4","VARNAME_4","TYPE_4","ENGTYPE_4","TYPE_3","ENGTYPE_3"]].drop_duplicates()
+layer4df["Layer"]=4
+layer4df["HASC_4"]="NA"
+#get Layer 3
+layer5df=gadmdata[["ID_5","ISO","NAME_4","NAME_5","TYPE_5","ENGTYPE_5","TYPE_4","ENGTYPE_4"]].drop_duplicates()
+layer5df["Layer"]=5
+layer5df["HASC_5"]="NA"
+layer5df["VARNAME_5"]="NA"
+#rename all dfs
+countrydf.columns=["ID","ISO","NAME","CountryISO","VARNAME","LOCALTYPE","ENGTYPE","ParentLOCALTYPE","ParentENGTYPE","Layer","Parent"]
+layer1df.columns=["ID","CountryISO","ISO","Parent","NAME","VARNAME","LOCALTYPE","ENGTYPE","Layer","ParentLOCALTYPE","ParentENGTYPE"]
+layer2df.columns=["ID","CountryISO","ISO","Parent","NAME","VARNAME","LOCALTYPE","ENGTYPE","ParentLOCALTYPE","ParentENGTYPE","Layer"]
+layer3df.columns=["ID","CountryISO","ISO","Parent","NAME","VARNAME","LOCALTYPE","ENGTYPE","ParentLOCALTYPE","ParentENGTYPE","Layer"]
+layer4df.columns=["ID","CountryISO","Parent","NAME","VARNAME","LOCALTYPE","ENGTYPE","ParentLOCALTYPE","ParentENGTYPE","Layer","ISO"]
+layer5df.columns=["ID","CountryISO","Parent","NAME","LOCALTYPE","ENGTYPE","ParentLOCALTYPE","ParentENGTYPE","Layer","ISO","VARNAME"]
+#append dataframes
+finaldf=countrydf.append(layer1df, ignore_index=True)
+finaldf=finaldf.append(layer2df, ignore_index=True)
+finaldf=finaldf.append(layer3df, ignore_index=True)
+finaldf=finaldf.append(layer4df, ignore_index=True)
+finaldf=finaldf.append(layer5df, ignore_index=True)
+#Create Aliases
+finaldf["Alias1"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"]
+finaldf["Alias2"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"].str[:3]
+finaldf["Alias3"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"]
+finaldf["Alias4"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"].str[:3]
+finaldf["Alias5"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"]+"_"+finaldf["NAME"].str[:3]
+finaldf["Alias6"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"].str[:3]+"_"+finaldf["NAME"].str[:3]
+finaldf["Alias7"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"]+"_"+finaldf["NAME"].str[:3]
+finaldf["Alias8"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"].str[:3]+"_"+finaldf["NAME"].str[:3]
+finaldf["Alias9"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"]+"_"+finaldf["NAME"]
+finaldf["Alias10"]=finaldf["CountryISO"]+"_"+finaldf["LOCALTYPE"].str[:3]+"_"+finaldf["NAME"]
+finaldf["Alias11"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"]+"_"+finaldf["NAME"]
+finaldf["Alias12"]=finaldf["CountryISO"]+"_"+finaldf["ENGTYPE"].str[:3]+"_"+finaldf["NAME"]
+finaldf["Alias13"]=finaldf["CountryISO"]+"_"+str(finaldf["Layer"])+"_"+finaldf["LOCALTYPE"]+"_"+finaldf["NAME"]
+finaldf["Alias14"]=finaldf["CountryISO"]+"_"+str(finaldf["Layer"])+"_"+finaldf["LOCALTYPE"].str[:3]+"_"+finaldf["NAME"]
+finaldf["Alias15"]=finaldf["CountryISO"]+"_"+str(finaldf["Layer"])+"_"+finaldf["ENGTYPE"]+"_"+finaldf["NAME"]
+finaldf["Alias16"]=finaldf["CountryISO"]+"_"+str(finaldf["Layer"])+"_"+finaldf["ENGTYPE"].str[:3]+"_"+finaldf["NAME"]
+
+#write putput to csv
+finaldf.to_csv(root+pathspt+ "files"+pathspt+"out.csv")
